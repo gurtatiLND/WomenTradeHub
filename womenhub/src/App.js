3939c2aefcbcf6e0c86b525ac5b55b4e614cebb8
GurTati <tatiana.gurova.j@gmail.com>
2024-06-12 17:05:54 +0100

    added a pic on main page

--- womenhub/src/App.js
+++ womenhub/src/App.js
@@ -1,22 +1,15 @@
-import { logo } from '../public/logo192.png';
+
 import './App.css';
+import picture from './main_pic.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={picture} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is just a picture of how a landing page should look like.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
